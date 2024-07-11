d32c601b2e148acc4c26bf8f1bc7aa078d5ae381
RafaelWasconcelos <rafaelwasconcelos10@gmail.com>
2024-07-11 19:05:12 -0300

    p3

--- exer_p3/c8.c
+++ exer_p3/c8.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+    struct tipoData
+    {
+        int ano,mes,dia;
+    };
+
+
     struct tipoPessoa 
     {
         char nome[80];
@@ -7,45 +13,26 @@
 
     };
 
-    struct tipoData
-    {
-        int ano,mes,dia;
-    };
-
 
     struct tipoPessoa separaInfo(char nome[80],int dt_nascimento)
     {
-        int i;
-        for(i=0;nome[i]!='\0';i)
+        struct tipoData v;
+        v.dia = ((dt_nascimento/10)%10)*10 + (dt_nascimento%10)*1;
+        v.mes = ((dt_nascimento/1000)%10)*10 + ((dt_nascimento/100)%10);
+        v.ano = ((dt_nascimento/10000000)%10)*1000 + ((dt_nascimento/1000000)%10)*100 +((dt_nascimento/100000)%10)*10 + ((dt_nascimento/10000)%10);
+        struct tipoPessoa a = {nome,v}; 
+    return a; 
     }
 
 
 
 
+    int main ()
+    {
+        struct tipoPessoa b = separaInfo("dourado henrique",19941012);
+        printf("%s\n",b.nome);
+        printf("%d\n",b.dtNascimento.dia);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main ()
-{
-
-
-
-}
+        return 0;
+    }
